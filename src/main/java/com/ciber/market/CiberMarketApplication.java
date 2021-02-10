4fbf384219025a8f41f0bcf0ad1c3a5416b7ce40
Ruben Alejandro Ruiz Alejandre <l18120192@morelia.tecnm.mx>
2021-02-10 16:24:44 -0600

    Arquitectura inicial del proyecto, mediante dominio

---
+++ src/main/java/com/ciber/market/CiberMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ciber.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiberMarketApplication.class, args);
+	}
+
+}
